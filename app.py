6c25a1a8b4e2c663c92a2d9a8d670410a426f1cf
Subba Raju Sarikonda <subbaraju6495@gmail.com>
2026-06-09 03:32:43 -0500

    Fix app file

--- app.py
+++ app.py
@@ -1,17 +1,52 @@
-question = st.text_input("Ask a question about your document:")
+import streamlit as st
+from pypdf import PdfReader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
 
-if question:
-    question_embedding = model.encode([question])
+st.title("AI Document Chatbot")
 
-    D, I = index.search(
-        np.array(question_embedding).astype("float32"),
-        k=3
+uploaded_file = st.file_uploader("Upload your PDF", type=["pdf"])
+
+if uploaded_file:
+    pdf_reader = PdfReader(uploaded_file)
+
+    text = ""
+    for page in pdf_reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text
+
+    st.success("PDF uploaded successfully!")
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50
     )
 
-    answer = ""
+    chunks = splitter.split_text(text)
+    st.write(f"Total Chunks Created: {len(chunks)}")
 
-    for idx in I[0]:
-        answer += chunks[idx] + "\n"
+    model = SentenceTransformer("all-MiniLM-L6-v2")
+    embeddings = model.encode(chunks).astype("float32")
 
-    st.subheader("Answer")
-    st.write(answer)
+    dimension = embeddings.shape[1]
+    index = faiss.IndexFlatL2(dimension)
+    index.add(embeddings)
+
+    st.success("Embeddings created and stored in FAISS!")
+
+    question = st.text_input("Ask a question about your document:")
+
+    if question:
+        question_embedding = model.encode([question]).astype("float32")
+
+        D, I = index.search(question_embedding, k=3)
+
+        answer = ""
+        for idx in I[0]:
+            answer += chunks[idx] + "\n\n"
+
+        st.subheader("Answer")
+        st.write(answer)

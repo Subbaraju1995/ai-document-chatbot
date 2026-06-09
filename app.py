008111b78172aec1fb1f24e3a3f292c1ae65c472
Subba Raju Sarikonda <subbaraju6495@gmail.com>
2026-06-09 02:52:11 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,65 @@
+import streamlit as st
+from pypdf import PdfReader
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from sentence_transformers import SentenceTransformer
+import faiss
+import numpy as np
+
+st.title("AI Document Chatbot")
+
+uploaded_file = st.file_uploader("Upload your PDF", type=["pdf"])
+
+if uploaded_file:
+
+    pdf_reader = PdfReader(uploaded_file)
+
+    text = ""
+
+    for page in pdf_reader.pages:
+        page_text = page.extract_text()
+        if page_text:
+            text += page_text
+
+    st.success("PDF uploaded successfully!")
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50
+    )
+
+    chunks = splitter.split_text(text)
+
+    st.write(f"Total Chunks Created: {len(chunks)}")
+
+    model = SentenceTransformer(
+        "all-MiniLM-L6-v2"
+    )
+
+    embeddings = model.encode(chunks)
+
+    dimension = embeddings.shape[1]
+
+    index = faiss.IndexFlatL2(dimension)
+
+    index.add(np.array(embeddings))
+
+    st.success("Embeddings created and stored in FAISS!")
+
+    question = st.text_input(
+        "Ask a question about your document:"
+    )
+
+    if question:
+
+        question_embedding = model.encode([question])
+
+        distances, indices = index.search(
+            np.array(question_embedding),
+            k=3
+        )
+
+        st.subheader("Relevant Information")
+
+        for idx in indices[0]:
+            st.write(chunks[idx])
